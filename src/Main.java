13405fc4811dedea9171f615bc75c5a1409b1e82
sohila elabasy <sohailaismail6@gmail.com>
2025-03-12 03:49:12 +0200

    sorting by names

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
-
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,32 +12,26 @@ public class Main {
     public static ArrayList<String> department = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("--------welcome to employees application--------");
-
         while (true) {
             System.out.println("1-Add Employee \n2-Display Employee\n3-Delete Employee \n4-Edit employee\n5-Calculate salaries \n6-sorting \n7-exit");
             System.out.print("your choice is :");
             Scanner input = new Scanner(System.in);
             int choice = input.nextInt();
             switch (choice) {
-                case 1:
-                    adding();break;
-                case 2:
-                    display();break;
-                case 3:
-                    delete();break;
-                case 4:
-                    edit();break;
-                case 5:
-                    calculateTotalSalaries();break;
-                case 7: break;
+                case 1:adding();break;
+                case 2:display();break;
+                case 3:delete();break;
+                case 4:edit();break;
+                case 5:calculateTotalSalaries();break;
+                case 6:sorting();break;
+                case 7:
+                    break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
-            if(choice==7)break;
+            if (choice == 7) break;
         }
     }
-
-
     static void adding() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter employee name");
@@ -48,13 +41,13 @@ public class Main {
 
         int empNum = input.nextInt();
         input.nextLine();
-        System.out.println("Enter employee employee address");
+        System.out.println("Enter employee address");
 
         String empAddress = input.nextLine();
-        System.out.println("Enter employee employee salary");
+        System.out.println("Enter employee salary");
 
         double empSalary = input.nextDouble();
-        System.out.println("Enter employee employee department");
+        System.out.println("Enter employee department");
         input.nextLine();
         String empDepartment = input.nextLine();
         names.add(empName);
@@ -63,32 +56,28 @@ public class Main {
         salary.add(empSalary);
         department.add(empDepartment);
     }
-
-
     static void display() {
         System.out.println("Please enter employee name...");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
-        if (!names.contains(name)){
-            System.out.println(name+" is not found");
+        if (!names.contains(name)) {
+            System.out.println(name + " is not found");
             return;
         }
-        int index= names.indexOf(name);
+        int index = names.indexOf(name);
         System.out.println("employee data ");
-            System.out.println("Name:" + names.get(index));
-            System.out.println("Phone:" + phone.get(index));
-            System.out.println("Address:" + address.get(index));
-            System.out.println("Salary:" + salary.get(index));
-            System.out.println("Department:" + department.get(index));
+        System.out.println("Name:" + names.get(index));
+        System.out.println("Phone:" + phone.get(index));
+        System.out.println("Address:" + address.get(index));
+        System.out.println("Salary:" + salary.get(index));
+        System.out.println("Department:" + department.get(index));
     }
-
-
     static void delete() {
         System.out.println("please enter the name of the employee...");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
-        int index= names.indexOf(name);
-        if(names.contains(name)){
+        int index = names.indexOf(name);
+        if (names.contains(name)) {
             names.remove(name);
             phone.remove(index);
             address.remove(index);
@@ -98,46 +87,43 @@ public class Main {
             System.out.println("Employee is deleted successfully");
         }
     }
-
-
-    static void edit(){
+    static void edit() {
         System.out.println("Plese enter tne employee name...");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
 
-        int index= names.indexOf(name);
+        int index = names.indexOf(name);
 
-        if(names.contains(name)){
+        if (names.contains(name)) {
             System.out.println("enter name");
-            String newName= input.nextLine();
+            String newName = input.nextLine();
 
             System.out.println("enter phone number");
-            int newPhone= input.nextInt();
+            int newPhone = input.nextInt();
             input.nextLine();
 
             System.out.println("enter address");
-            String newAddress= input.nextLine();
+            String newAddress = input.nextLine();
 
             System.out.println("enter salary");
-            double newSalary=input.nextDouble();
+            double newSalary = input.nextDouble();
             input.nextLine();
 
             System.out.println("enter department");
-            String newDepartment= input.nextLine();
+            String newDepartment = input.nextLine();
 
-            names.set(index,newName);
-            phone.set(index,newPhone);
-            address.set(index,newAddress);
-            salary.set(index,newSalary);
-            department.set(index,newDepartment);
+            names.set(index, newName);
+            phone.set(index, newPhone);
+            address.set(index, newAddress);
+            salary.set(index, newSalary);
+            department.set(index, newDepartment);
 
             System.out.println("Employee is updated successfully");
 
         }
 
     }
-
-    static void calculateTotalSalaries(){
+    static void calculateTotalSalaries() {
         System.out.println("Plese enter tne Department...");
         Scanner input = new Scanner(System.in);
         String depart = input.nextLine();
@@ -151,24 +137,43 @@ public class Main {
         }
         System.out.println("Total Salaries for " + depart + " department: " + totalSalaries);
     }
-    static void sorting(){
+    static void sorting() {
         System.out.println("choose the type of sorting...");
         System.out.println("1-sorting by names \n2-sorting by phone number\n3-sorting by department");
         Scanner input = new Scanner(System.in);
-        int sortType=input.nextInt();
-        switch (sortType){
-            case 1:sortingByNames();break;
-            case 2:sortingByPhoneNumber();break;
-            case 3:sortingByDepartment();break;
+        int sortType = input.nextInt();
+        switch (sortType) {
+            case 1:
+                sortingByNames();
+                break;
+            case 2:
+                sortingByPhoneNumber();
+                break;
+            case 3:
+                sortingByDepartment();
+                break;
         }
     }
-    static void sortingByNames(){
+    static void sortingByNames() {
+       ArrayList<String>sorted= new ArrayList<>();
+       for (String name:names){
+           sorted.add(name);
+       }
+       Collections.sort(sorted);
+        for(String sort:sorted){
+            for(String name :names){
+                if(name.equals(sort)){
+                    int index = names.indexOf(name);
+                    System.out.println("employee data is :");
+                    System.out.println(names.get(index));
+                    System.out.println(phone.get(index));
+                    System.out.println(address.get(index));
+                    System.out.println(salary.get(index));
+                    System.out.println(department.get(index));
+                }
+            }
 
-    }
-    static void sortingByPhoneNumber(){
-
-    }
-    static void sortingByDepartment(){
+        }
 
     }
 
